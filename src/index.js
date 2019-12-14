a1fec512b8e482d70adce55cba43d74cddc7227b
Singlesaul <singlesaulwork@gmail.com>
2019-12-14 23:52:25 +0800

    全部提交

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './default.css'
+import './style-one.scss'
+import './style-two.less'
+import './style-three.styl'
+import png from './assets/one.png'
+
+const logo = document.getElementById('logo')
+logo.innerHTML = `
+  <img src="${png}">
+`
+
+const div = document.getElementById('lazy-load')
+const button = document.createElement('button')
+
+button.innerText = 'I am Lazy-Load'
+
+button.onclick = () => {
+  const promise = import('./lazy')
+  promise.then((module)=>{
+    module.default()
+  },()=>{
+    console.log('Error')
+  })
+}
+
+div.appendChild(button)
---
+++ src/lazy.js
@@ -0,0 +1,4 @@
+export default function lazyModule() {
+    console.log('This is a lazy loading module.')
+    alert('This is a lazy loading module.')
+  }
